a7a288e753bb3d67a4f2dcd093eeae26455b5aac
Julia2906 <137899526+Julia2906@users.noreply.github.com>
2025-03-27 22:59:16 +0200

    add getItem to useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,17 +5,20 @@ import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
+  const [contacts, setContacts] = useState(
+    () =>
+      JSON.parse(localStorage.getItem('saved-contacts')) || [
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]
+  );
 
   const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    window.localStorage.setItem('saved-contacts', JSON.stringify(contacts));
+    localStorage.setItem('saved-contacts', JSON.stringify(contacts));
   }, [contacts]);
 
   const filteredContacts = contacts.filter(contact =>
